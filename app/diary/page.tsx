1794ccd83f1faf273b8b0891f196076062c33be9
eunah <eunah3693@naver.com>
2026-05-10 12:23:48 +0900

    feat: diary loading 추가

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -17,7 +17,6 @@ export default async function DiaryListPage() {
 
   if (user) {
     const diaryService = new DiaryService();
-
     await queryClient.prefetchInfiniteQuery({
       queryKey: ['diary'],
       queryFn: ({ pageParam = 0 }) =>
